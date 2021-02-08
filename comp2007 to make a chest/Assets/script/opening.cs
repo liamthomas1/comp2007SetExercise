a1aa33c594aa8942d58bed8cb5d87f3c66bbd62b
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2021-02-08 13:31:50 +0000

    part 5
    their is a baics code to open and close the chest
    the chest open with the key press of up for up and down for down
    other chest that was made frist is being brought back as well

--- comp2007 to make a chest/Assets/script/opening.cs
+++ comp2007 to make a chest/Assets/script/opening.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class opening : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public float turnspeed = 50f;
     // Update is called once per frame
+    //this is a test for this
+    public float open = -170;
+    public float close = -60;
+    
     void Update()
     {
-        
+
+        // this is how the lid is being open and close 
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(Vector3.left, turnspeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(Vector3.right, turnspeed * Time.deltaTime);
+
     }
 }
