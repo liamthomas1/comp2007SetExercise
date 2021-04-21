e30f017786eb3b43d7bc11691cfe3e3754f367b7
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2021-04-21 02:04:56 +0100

    part 11
    add a new button for each sense so you can move to each page staring in main menu to chest to ui then sound this is done to make more simple for people see this who don't understand what it is and how it work.
    also add text to top left of ui so you can see what you look at

---
+++ comp2007 to make a chest/Assets/script/loadscenes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class loadscenes : MonoBehaviour
+    
+{
+    public string scenesname;
+    // Start is called before the first frame update
+    public void nextscens()
+	{
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void Update()
+	{
+		 if
+	}
+}
